184a28e3ee23b3007472660818ef3a49f4a68f85
jayhug2 <jayhug2@daum.net>
2022-03-17 19:17:15 +0900

    220317 reactbook study

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import EventPractice from "./EventPractice";
+import { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
 
-const App = () => {
-  return (
-    <EventPractice />
-  )
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000"
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+        <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
+      </div>
+    )
+  }
 };
 
 
